f7ca60d4a012f53bbb2544718286bece02bb2f9f
Phif <philippesilva.94@gmail.com>
2024-04-12 02:28:42 +0200

    feat: dictionary

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("hello")
+fetch("https://api.datamuse.com/words?sp=*&md=p&max=1000")
+.then(response => response.json())
+.then(data => {
+    const adjectives = data.filter(word => word.tags && word.tags.includes('adj'));
+    console.log(adjectives);
+})
+.catch(e => console.log("Error:", e))
